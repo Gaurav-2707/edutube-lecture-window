1ec9fa313a15c56127d80485e16e43784d5dc4a8
Gaurav-2707 <gauravsrivastava2707@gmail.com>
2024-10-08 04:52:36 +0530

    change heading

--- app/component/Overview.jsx
+++ app/component/Overview.jsx
@@ -4,7 +4,7 @@ const Overview = () => {
   return (
     <div>
         <div className='text-black absolute top-32 left-1/4'>
-            <h1 className='text-4xl pb-4'>COURSE OVERVIEW</h1>
+            <h1 className='text-4xl pb-4'>Information and Communication Theory</h1>
             <p>The goal of this course is to provide students with an introduction to the key concepts and techniques used in Information and Communication Technology, with an emphasis on the practical application of technologies and systems that support information processing and exchange. Additionally, students will gain proficiency in ICT tools and environments for effective communication and data management.</p>
             <br/>
             <p><b>Syllabus:</b><br/><b>Networking and Data Transmission:</b> Fundamentals of computer networks, transmission media, types of networks (LAN, WAN, MAN), protocols (TCP/IP, HTTP, FTP), IP addressing, and subnetting.<br/><b>Information Systems and Databases:</b> Concepts of information systems, database management systems (DBMS), relational databases, SQL for data retrieval and manipulation, entity-relationship diagrams, and normalization<br/><b>Cybersecurity:</b> Principles of cybersecurity, types of cyber threats (malware, phishing, DDoS), encryption, firewalls, VPNs, and best practices for securing networks and information systems.<br/><b>ICT Applications and Trends:</b> Emerging technologies in ICT (IoT, AI, Blockchain), their application in business, education, and health sectors, and the ethical considerations surrounding data privacy and digital security.</p>
